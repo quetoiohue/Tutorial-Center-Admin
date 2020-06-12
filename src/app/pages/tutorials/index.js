f3977383eaf86c404d755b68e0139f2bf3f25a69
gwangchan <quetoiohue@gmail.com>
2020-06-12 21:14:26 +0700

    modify ttutorial management page

--- src/app/pages/tutorials/index.js
+++ src/app/pages/tutorials/index.js
@@ -11,40 +11,64 @@ import CustomizedIconButton from "../../partials/content/CustomizedIconButton";
 import Rating from "../../partials/content/Socials/Rating";
 import MatTable from "../../partials/content/Table";
 import DeleteModal from "./components/DeleteModal";
+import tutorialActions from "../../store/ducks/actions/tutorials";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
+import LoadingProgress from "../../components/LoadingProgress";
 
 const Tutorials = () => {
+  const dispatch = useDispatch();
   const [modal, setModal] = React.useState({
     type: "",
     selected: {},
   });
-
-  const dataRows = rowsData.map((_item, index) => {
-    return createData(
-      _item.id,
-      _item.post_id,
-      _item.author_id,
-      _item.title,
-      <Rating number={_item.rates} fontSize="small" />,
-      _item.last_modified,
-      _item.last_created,
-      <>
-        <CustomizedIconButton
-          onClick={() => hasDeleteTutorialModal("delete", _item)}
-          Icon={<Delete />}
-          title="Delete"
-        />
-        {false ? (
-          <CustomizedIconButton Icon={<AddBox />} title="Unblock" />
-        ) : (
-          <CustomizedIconButton Icon={<RemoveCircle />} title="Block" />
-        )}
-        <Link to={`/tutorials/1`}>
-          <CustomizedIconButton Icon={<ArrowRightAlt />} title="Detail" />
-        </Link>
-      </>
-    );
+  const [pagination, setPagination] = React.useState({
+    offset: 0,
+    limit: 10,
   });
-  console.log(rowsData);
+  const { tutorials, count, isFetching } = useSelector((store) => store.tutorialList);
+  const { offset, limit } = pagination;
+  const { type } = modal;
+
+  React.useEffect(() => {
+    dispatch(tutorialActions.getTutorials({ offset, limit }));
+  }, [offset, limit]);
+
+  const dataRows = React.useMemo(() => {
+    if (!tutorials) return [];
+    return tutorials.map((_item, index) => {
+      const averagingRate =
+        _item.rates.length &&
+        _item.rates.reduce((sum, rate) => sum + rate.score, 0) /
+          _item.rates.length;
+      return createData(
+        _item.id,
+        _item.id,
+        _item.name,
+        _item.title,
+        <Rating number={Number.parseInt(averagingRate)} fontSize="small" />,
+        moment(_item.updated_at).format("DD/MM/YYYY"),
+        moment(_item.created_at).format("DD/MM/YYYY"),
+        <>
+          <CustomizedIconButton
+            onClick={() => hasDeleteTutorialModal("delete", _item)}
+            Icon={<Delete />}
+            title="Delete"
+          />
+          {false ? (
+            <CustomizedIconButton Icon={<AddBox />} title="Unblock" />
+          ) : (
+            <CustomizedIconButton Icon={<RemoveCircle />} title="Block" />
+          )}
+          <Link to={`/tutorials/${_item.id}`}>
+            <CustomizedIconButton Icon={<ArrowRightAlt />} title="Detail" />
+          </Link>
+        </>
+      );
+    });
+  }, [tutorials, offset, limit]);
+
+  console.log(dataRows);
 
   const hasDeleteTutorialModal = (type, selected) => {
     setModal((prevState) => ({
@@ -66,14 +90,19 @@ const Tutorials = () => {
     console.log("deleting submit");
   };
 
-  const { type } = modal;
-
   return (
     <>
       <div style={{ display: "flex" }}>
         <h1>Tutorial management</h1>
       </div>
-      <MatTable headRows={headRows} rows={dataRows} />
+      <MatTable
+        headRows={headRows}
+        rows={dataRows}
+        pagination={pagination}
+        setPagination={setPagination}
+        count={count}
+        isFetching={isFetching}
+      />
       {
         <DeleteModal
           open={type === "delete"}
